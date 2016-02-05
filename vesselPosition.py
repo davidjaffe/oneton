bd020f518ef5bf7d2beecb8b2a5533fce9511648
David Jaffe <djaffe@bnl.gov>
2016-02-05 10:57:05 -0500

    fix msmts of vessel position, add msmts of pmt positions, begin adding msmts of hodos

--- vesselPosition.py
+++ vesselPosition.py
@@ -2,7 +2,8 @@
 '''
 convert measured positions of vessel to positions in x,y
 20151218
-20150201 more work
+20160201 more work
+20160204 add bottom PMT positions
 '''
 
 import csv
@@ -19,10 +20,15 @@ barINCH    = 1.5  # width of 80-20 bar in inches
 offset = offsetINCH * 2.54 # width and length of platform
 bar    = barINCH*2.54   # width of 80-20 bar
 halfbar = bar/2.
+fpt   = 0.25*2.54 # 80-20 fastener plate thickness in cm
+Rmushield = 6.0/2.# mu-metal shield radius from R7723 ass'y drawing
+Rpmt = 5.2/2. # nominal radius of PMT window from R7723 ass'y drawing (diameter=52 +-1.5mm)
+dQpmt = 25. # spacing set between bottom PMTs
 
 Rdesign = 104.58/2. # design OD/2
 
 print 'width of platform',offset,'bar',bar,'design Radius',Rdesign
+print 'fastener plate thickness',fpt,'radius of R7723 PMT mu-metal shield',Rmushield,'radius of R7723 PMT window',Rpmt,'spacing btwn bottom PMTs',dQpmt
 
 ## read measurement data from spread sheet
 fn = 'Measured_positions_book3p35_20151218.csv'
@@ -157,59 +163,79 @@ U7 = [x*2.54 for x in U7] # convert to cm
 V7 = [20.0,16.1,13.6,12.3,12.3,13.2,15.4,19.0] # cm
 sV7 = [0.3 for x in U7]
 
-## calculate translation of U,V measurements to X,Y
-## requires extrapolation of platform x-positions (called r,l in spreadsheet) to table x-positions
-print 'offset',offset,'halfbar',halfbar
-sprime = [offset/2.-x-halfbar for x in columns['scm'][:lastIndex]]
-mprime = [x+halfbar-offset/2. for x in columns['mcm'][:lastIndex]]
-rprime = columns['rcm'][:lastIndex]
-lprime = columns['lcm'][:lastIndex]
-print 'sprime',sprime
-print 'rprime',rprime
-print 'mprime',mprime
-print 'lprime',lprime
+## 20160204 new translations of U,V measurements to X,Y
+Xuv,Yuv = [],[]
+dXback = 0.5*(offset + columns['rcm'][0] - columns['lcm'][0])
+dY = 0.5*(columns['tcm'][0] + columns['tcm'][lastIndex-1])
+for u,v in zip(U1,V1):
+    Xuv.append( dXback - u )
+    Yuv.append( offset - v - dY )
+sXuv,sYuv = [],[]
+sXuv.extend( sU1 )
+sYuv.extend( sV1 )
+dXfront = 0.5*(offset + columns['rcm'][lastIndex-1] - columns['lcm'][lastIndex-1])
+for u,v in zip(U7,V7):
+    Xuv.append( dXfront - u )
+    Yuv.append( v - dY )
+sXuv.extend( sU7 )
+sYuv.extend( sV7 )    
 
-Slope,Intercept = {},{}
-for Side in ['right','left']:
-    R,S = rprime,sprime
-    if Side=='left': R,S = lprime,mprime
+useOldUV = False
+if useOldUV:
 
-    Srr,Sr,Ss,Srs = 0.,0.,0.,0.
-    for r,s in zip(R,S):
-        Sr += r
-        Srr+= r*r
-        Ss += s
-        Srs+= r*s
-    N = float(len(rprime))
-    slope = (Sr*Sr - N*Srr)/(Sr*Ss - N*Srs)
-    intercept = (Sr - slope*Srs)/N
-    x0 = slope*offset/2.+intercept
-    x7 = -slope*offset/2.+intercept
-    print Side,'side: slope',slope,'intercept',intercept,'x0',x0,'x7',x7
-    Slope[Side],Intercept[Side] = slope,intercept
+    ## calculate translation of U,V measurements to X,Y
+    ## requires extrapolation of platform x-positions (called r,l in spreadsheet) to table x-positions
+    print 'offset',offset,'halfbar',halfbar
+    sprime = [offset/2.-x-halfbar for x in columns['scm'][:lastIndex]]
+    mprime = [x+halfbar-offset/2. for x in columns['mcm'][:lastIndex]]
+    rprime = columns['rcm'][:lastIndex]
+    lprime = columns['lcm'][:lastIndex]
+    print 'sprime',sprime
+    print 'rprime',rprime
+    print 'mprime',mprime
+    print 'lprime',lprime
 
-    
-# use right side to correct U1,V1, U7,V7
-Xq,Yq,sXq,sYq = [],[],[],[]
-for u1,v1 in zip(U1,V1):
-    delta = Slope['right']*(-offset/2.) + Intercept['right']
-    x = u1 - delta - offset/2.
-    y = offset/2. - v1
-    Xq.append(x)
-    Yq.append(y)
-sXq.extend( sU1 )
-sYq.extend( sV1 )
-for u7,v7 in zip(U7,V7):
-    delta = Slope['right']*(offset/2.) + Intercept['right']
-    x = u7 - delta - offset/2.
-    y = -offset/2. + v7
-    Xq.append(x)
-    Yq.append(y)
-sXq.extend( sU7 )
-sYq.extend( sV7 )
+    Slope,Intercept = {},{}
+    for Side in ['right','left']:
+        R,S = rprime,sprime
+        if Side=='left': R,S = lprime,mprime
 
-Xqave = sum(Xq)/float(len(Xq))
-Yqave = sum(Yq)/float(len(Yq))
+        Srr,Sr,Ss,Srs = 0.,0.,0.,0.
+        for r,s in zip(R,S):
+            Sr += r
+            Srr+= r*r
+            Ss += s
+            Srs+= r*s
+        N = float(len(rprime))
+        slope = (Sr*Sr - N*Srr)/(Sr*Ss - N*Srs)
+        intercept = (Sr - slope*Srs)/N
+        x0 = slope*offset/2.+intercept
+        x7 = -slope*offset/2.+intercept
+        print Side,'side: slope',slope,'intercept',intercept,'x0',x0,'x7',x7
+        Slope[Side],Intercept[Side] = slope,intercept
+
+
+    # use right side to correct U1,V1, U7,V7
+    Xq,Yq,sXq,sYq = [],[],[],[]
+    for u1,v1 in zip(U1,V1):
+        delta = Slope['right']*(-offset/2.) + Intercept['right']
+        x = u1 - delta - offset/2.
+        y = offset/2. - v1
+        Xq.append(x)
+        Yq.append(y)
+    sXq.extend( sU1 )
+    sYq.extend( sV1 )
+    for u7,v7 in zip(U7,V7):
+        delta = Slope['right']*(offset/2.) + Intercept['right']
+        x = u7 - delta - offset/2.
+        y = -offset/2. + v7
+        Xq.append(x)
+        Yq.append(y)
+    sXq.extend( sU7 )
+    sYq.extend( sV7 )
+
+    Xqave = sum(Xq)/float(len(Xq))
+    Yqave = sum(Yq)/float(len(Yq))
 
 # put information into more convenient form
 X,Y = [],[]
@@ -238,12 +264,20 @@ for i,x in enumerate(X):
         sX.append(1./16.*2.54)
 
 # append additional measurements and uncertainties
-useAddlMsmts = False
+useAddlMsmts = True
+useNewUV = True
 if useAddlMsmts:
-    X.extend(Xq)
-    Y.extend(Yq)
-    sX.extend(sXq)
-    sY.extend(sYq)
+    if useNewUV:
+        X.extend(Xuv)
+        Y.extend(Yuv)
+        sX.extend(sXuv)
+        sY.extend(sYuv)
+    else:
+        if useOldUV:
+            X.extend(Xq)
+            Y.extend(Yq)
+            sX.extend(sXq)
+            sY.extend(sYq)
 
 
 #print 'X',X,'Y',Y
@@ -312,8 +346,8 @@ for j,x in enumerate(X):
     chi2 += residual
     Phi.append(phi)
     Residual.append(residual)
-    print 'x,y,phi,residual',x,y,phi,residual
-print 'chi2',chi2
+    #print 'x,y,phi,residual',x,y,phi,residual
+print 'chi2',chi2,'for',len(X),'points and',len(bestRxy),'parameters'
 
 calcHorizDisp = False
 if calcHorizDisp:
@@ -330,17 +364,67 @@ if calcHorizDisp:
             dx0 = x-xp
         C.append(y)
         D.append(abs(dx0))
-        s = math.sqrt(Rdesign*Rdesign - (y-Yqave)*(y-Yqave))
-        xp = Xqave+s
-        xm = Xqave-s
-        dxq = x-xm
-        if abs(x-xp)<abs(x-xm):
-            dxq = x-xp
+        if useOldUV:
+            s = math.sqrt(Rdesign*Rdesign - (y-Yqave)*(y-Yqave))
+            xp = Xqave+s
+            xm = Xqave-s
+            dxq = x-xm
+            if abs(x-xp)<abs(x-xm):
+                dxq = x-xp
         print 'y,x',y,x,'dx0',dx0,'dxq',dxq
     print ''
 
-# draw a bunch of stuff
-rf = TFile('vP.root','RECREATE')
+### positions of bottom PMTs 20160204
+### Assume PMTs in contact with bottom of vessel and bottom of vessel defines Z=0
+dY = bar/2. + fpt + bar + fpt + bar + Rmushield
+y = columns['tcm'][2] - 0.5*(columns['tcm'][0]+columns['tcm'][lastIndex-1]) 
+Ypmt = [y - dY for i in range(4)] # all bottom PMTs at same Y position
+Xpmt = []
+x = columns['rcm'][2] - columns['lcm'][2] + 1.5*dQpmt
+Xpmt.append(x)
+for i in range(3):
+    x -= dQpmt
+    Xpmt.append(x)
+Zpmt = [0. for i in range(len(Xpmt))]
+
+### top PMTs
+Xleft = -(offset - columns['rcm'][2] + columns['lcm'][2])/2.
+x4 = x5 = Xleft + 41.6 + bar + fpt + bar/2.
+Xpmt.extend([x4,x5])
+Yback = offset - 0.5*(columns['ncm'][0] + columns['ncm'][lastIndex-1])
+y4 = Yback - (26.+9./16.)*2.54 + bar/2. + 35.8 - bar - Rmushield
+y5 = Yback - (26.+9./16.)*2.54 + bar/2  - 18.7 + bar + Rmushield
+Ypmt.extend([y4,y5])
+Ztop = 130.4925
+Zpmt.extend([Ztop,Ztop])
+
+### positions of bottom hodoscopes H4, H5 (give X,Y,Z of corners starting at back right)
+### H5
+Xright = 0.5*(offset + columns['rcm'][0] - columns['lcm'][0])
+xbr = Xright - (8.+9./16.)*2.54
+xbl = Xright - (20.5)*2.54
+xfr = Xright - (8.5)*2.54
+xfl = Xright - (20.+7./16.)*2.54
+Yback = 0.5*(columns['scm'][lastIndex-1] - columns['scm'][0])
+ybr = Yback + bar/2. + bar - (12.+7./8.)*2.54
+ybl = Yback + bar/2. + bar - (12.+15./16.)*2.54
+Yfront = 0.5*(columns['scm'][0] - columns['scm'][lastIndex-1])
+yfr = Yfront - bar/2. - bar + (2.5)*2.54
+yfl = Yfront - bar/2. - bar + (2.5)*2.54
+Ztop = (3.5 - 27.8625)*2.54
+Zbot = Ztop - (0.4)*2.54
+Hodos = {}
+Hodos['H5'] = [ [xbr,ybr,Ztop], [xfr,yfr,Ztop], [xfl,yfl,Ztop], [xbl,ybl,Ztop], \
+                [xbr,ybr,Zbot], [xfr,yfr,Zbot], [xfl,yfl,Zbot], [xbl,ybl,Zbot] ]
+    
+
+
+                                                
+
+############# draw a bunch of stuff
+############# and write graphs, hists, etc. to root file
+rfn = 'vP.root'
+rf = TFile(rfn,'RECREATE')
 
 
 if calcHorizDisp:
@@ -379,27 +463,71 @@ for i in range(nphi):
     Yfit.append(y)
     Xd.append( 0. + Rdesign*math.cos(phi) )
     Yd.append( 0. + Rdesign*math.sin(phi) )
-    Xduv.append( Xqave + Rdesign*math.cos(phi) )
-    Yduv.append( Yqave + Rdesign*math.sin(phi) )
+    if useOldUV:
+        Xduv.append( Xqave + Rdesign*math.cos(phi) )
+        Yduv.append( Yqave + Rdesign*math.sin(phi) )
 name = 'fitted_circle'
 title = name.replace('_',' ')
+cR = '{0:.2f}'.format(Rguess)
+cx0= '{0:.2f}'.format(x0)
+cy0= '{0:.2f}'.format(y0)
+formula = cR + '^{2} = (x - '+cx0+')^{2} + (y - '+cy0+')^{2}'
+title += ' ' + formula
 g = gU.makeTGraph(Xfit,Yfit,title,name)
 gU.color(g,2,2,setMarkerColor=False,setMarkerType=False)
 tmg.Add(g)
 rf.WriteTObject(g)
 
+# draw  PMTs
+phi0 = -math.pi
+nphi = 100
+dphi = 2.*math.pi/float(nphi)
+for iPMT in range(len(Xpmt)):
+    xc,yc = Xpmt[iPMT],Ypmt[iPMT]
+    name = title = 'PMT'+str(iPMT)
+    Xarc,Yarc = [],[]
+    for i in range(nphi):
+        phi = phi0+float(i)+dphi
+        Xarc.append( xc + Rpmt*math.cos(phi) )
+        Yarc.append( yc + Rpmt*math.sin(phi) )
+    g = gU.makeTGraph(Xarc,Yarc,title,name)
+    gU.color(g,0+iPMT,0+iPMT,setMarkerColor=False,setMarkerType=False)
+    tmg.Add(g)
+    rf.WriteTObject(g)
+
+# draw hodoscopes
+icol = 10
+for H in Hodos:
+    XYZ = Hodos[H]
+    Xh,Yh = [],[]
+    for xyz in XYZ:
+        Xh.append(xyz[0])
+        Yh.append(xyz[1])
+    name = title = H
+    g = gU.makeTGraph(Xh,Yh,title,name)
+    icol += 1
+    gU.color(g,icol,icol,setMarkerColor=True,setMarkerType=False)
+    g.SetLineColor(icol) 
+    tmg.Add(g)
+    rf.WriteTObject(g)
+
 name = title = 'design'
+cR = '{0:.2f}'.format(Rdesign)
+formula = cR + '^{2} = x^{2} + y^{2}'
+title += ' ' + formula
 g = gU.makeTGraph(Xd,Yd,title,name)
 gU.color(g,4,4,setMarkerColor=False,setMarkerType=False)
 tmg.Add(g)
 rf.WriteTObject(g)
 
-name = 'design_centered_at_uv_average'
-title = name.replace('_',' ')
-g = gU.makeTGraph(Xduv,Yduv,title,name)
-gU.color(g,5,5,setMarkerColor=False,setMarkerType=False)
-tmg.Add(g)
-rf.WriteTObject(g)
+showDesignCenteredAtUV = False
+if showDesignCenteredAtUV:
+    name = 'design_centered_at_uv_average'
+    title = name.replace('_',' ')
+    g = gU.makeTGraph(Xduv,Yduv,title,name)
+    gU.color(g,5,5,setMarkerColor=False,setMarkerType=False)
+    tmg.Add(g)
+    rf.WriteTObject(g)
 
 name = 'measured_points'
 title = name.replace('_',' ')
@@ -408,17 +536,27 @@ gU.color(g,0,0,setMarkerColor=True)
 g.SetLineColor(10) # white lines
 tmg.Add(g)
 
-name = 'new_measured_points'
-title = name.replace('_',' ')
-g = gU.makeTGraph(newX,newY,title,name)
-gU.color(g,2,2,setMarkerColor=True)
-g.SetLineColor(10) # white lines
-tmg.Add(g)
+if 0: # no need, since replaced old with new above
+    name = 'new_measured_points'
+    title = name.replace('_',' ')
+    g = gU.makeTGraph(newX,newY,title,name)
+    gU.color(g,2,2,setMarkerColor=True)
+    g.SetLineColor(10) # white lines
+    tmg.Add(g)
 
-name = 'UV_measured_points'
+show_old_UV = useOldUV
+if show_old_UV:
+    name = 'UV_measured_points'
+    title = name.replace('_',' ')
+    g = gU.makeTGraph(Xq,Yq,title,name)
+    gU.color(g,3,3,setMarkerColor=True)
+    g.SetLineColor(10) # white lines
+    tmg.Add(g)
+
+name = 'new_UV_measured_points'
 title = name.replace('_',' ')
-g = gU.makeTGraph(Xq,Yq,title,name)
-gU.color(g,3,3,setMarkerColor=True)
+g = gU.makeTGraph(Xuv,Yuv,title,name)
+gU.color(g,13,13,setMarkerColor=True)
 g.SetLineColor(10) # white lines
 tmg.Add(g)
 
@@ -429,3 +567,4 @@ rf.WriteTObject(g)
 rf.WriteTObject(tmg)
 
 rf.Close()
+print 'Closed',rfn
