3478b0a56b5b5cc4c60b317047762bc4f6dbe14b
David Jaffe <djaffe@bnl.gov>
2016-03-13 15:27:24 -0400

    used for 20160313 estimates

--- cosmictele.py
+++ cosmictele.py
@@ -17,7 +17,6 @@ class cosmictele():
         Nlayers = 4
         
         Nmeas = Nlayers
-        fNmeas= float(Nmeas)
         sig   = 1./math.sqrt(12.)
         y0 = 0.
         z0 = 0.
@@ -31,6 +30,7 @@ class cosmictele():
             Z.extend([z0+dzplane2+float(i)*dz for i in range(Nlayers)])
             Z0plane.append(Z[Nlayers])
                            
+        fNmeas= float(Nmeas)
         Z = numpy.array(Z)
         Sz = sum(Z)
         Szz= sum(Z*Z)
@@ -38,6 +38,7 @@ class cosmictele():
 
         yave = yrms = 0.
         slope = slrms = 0.
+        bave = brms  = 0.
         for e in range(Nevt):
             Y = numpy.random.normal(y0, sig, Nmeas)
             #if e==0: print 'Y',Y
@@ -50,17 +51,25 @@ class cosmictele():
             yrms += y*y
             slope += m
             slrms += m*m
+            bave += b
+            brms += b*b
         yave = yave/float(Nevt)
         yrms = math.sqrt( (yrms-yave*yave)/float(Nevt-1) )
         slope = slope/float(Nevt)
         slrms = math.sqrt( (slrms-slope*slope)/float(Nevt-1) )
+        bave = bave/float(Nevt)
+        brms = math.sqrt( (brms-bave*bave)/float(Nevt-1) )
         words = 'Nevt '+ str(Nevt)+' #planes '+str(Nplane)+' Z0plane '
-        words += ' '.join('%.3f'% x for x in Z0plane)
+        words += ' '.join('%.1f'% x for x in Z0plane)
         words += ' yave,yrms,zextrap '
         words += ' '.join('%.3f'% x for x in [yave,yrms,zextrap])
-        words += 'yres {0:.3f}'.format(sig)
+        words += ' slope,sloperms '
+        words += ' '.join('%.5f'% x for x in [slope,slrms])
+        words += ' intercept,intrms '
+        words += ' '.join('%.5f'% x for x in [b,bave])
+        words += ' yres {0:.3f}'.format(sig)
         print words
-        #print '#planes',Nplane,'Z0plane',Z0plane,'Nevt,yave,yrms',Nevt,yave,yrms,'for zextrap',zextrap,'yrms/zextrap',yrms/zextrap,'z separation of planes',dz,'yres',sig
+        #print '#planes',Nplane,'Z0plane',Z0plane,'Nevt,yave,yrms',Nevt,yave,yrms,'for zv',zv,'yrms/zextrap',yrms/zextrap,'z separation of planes',dz,'yres',sig
         return
 if __name__ == '__main__' :
 
@@ -68,7 +77,8 @@ if __name__ == '__main__' :
 
     for nplane in [1,2]:
         dzplaneList = [0.]
-        if nplane==2: dzplaneList = [-100., 600., 1000., 151.]
+        if nplane==2: dzplaneList = [-100., 600., 1000.]
         for z in [50.,  150.,  250.]:
+            print ''
             for dzplane in dzplaneList:
                 CT.doit(Nevt=100000,zextrap=z,Nplane=nplane,dzplane2=dzplane)
