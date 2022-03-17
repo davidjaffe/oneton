96b32a43cad503f4397b989ec6f2d27a904016e6
David Jaffe <david.ezekial.jaffe@gmail.com>
2022-03-17 16:54:59 -0400

    improve chisquare minimization procedure and develop method to save results, read multiple results back, combine and plot them

--- THREE/calibf.py
+++ THREE/calibf.py
@@ -12,10 +12,38 @@ import os
 import sys
 import math
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker # https://stackoverflow.com/questions/12608788/changing-the-tick-frequency-on-x-or-y-axis-in-matplotlib/19972993#19972993
+
+import datetime
+import Logger
+
+import pickle
+import copy
 
 class calibf():
     def __init__(self):
 
+
+        now = datetime.datetime.now()
+        self.now = now.strftime('%Y%m%dT%H%M%S')
+
+        self.rootDir = 'JOBS_CALIBF/'
+        parentDir = self.rootDir+self.now
+        dirs = [parentDir]
+        self.Figures = parentDir  + '/FIGURES/'
+        self.logDir = parentDir
+        dirs.append( self.Figures)
+        dirs.append( self.logDir)
+
+        for d in dirs:
+            if not os.path.exists(d):
+                os.makedirs(d)
+                print('calibf.__init__ create directory',d)
+
+        lf = self.logDir + '/logfile.log'
+        sys.stdout = Logger.Logger(fn=lf)
+        print('calibf.__init__ Output directed to stdout and',lf)
+        
         self.speModels = {'DEFAULT': (1., 1./2.35)} # (mean, sigma)
 
         self.binwidth  = 1.0 # for NPE distributions with electronics response
@@ -75,15 +103,16 @@ class calibf():
             fNPE_gen.append( fNPE )
         
         return numpy.array(NPE_gen), numpy.array(fNPE_gen)
-    def fitExpt(self,fNPE,model='DEFAULT',muMC=None,PLOT=True,allSteps=False,useLogLike=False):
+    def fitExpt(self,fNPE,model='DEFAULT',muMC=None,PLOT=True,allSteps=False,useLogLike=False,debug=-1,comment=''):
         '''
         return bfNPE, cf given input NPE distribution fNPE where
         bfNPE = best fit NPE distribution
         cf  = calibration factor applied to MC to give best fit
         '''
-        debug = 0
         
         meanmu = numpy.mean(fNPE)
+        words = 'fitExpt_meanData_{:}_meanMC_{:}permille'.format(int(1000*meanmu),int(1000*muMC)) + comment
+        if comment!='' : print('calibf.fitExpt',words)
 
         minCF = 0.2
         maxCF = 2.2
@@ -102,9 +131,13 @@ class calibf():
         NMC   = int(rmcd)*Ndata
         
         ## use bins >> maximum of input data to ensure that full range of calibration can be explored
+        ## add large overflow bin instead of increasing overflow bins
         binwidth = self.binwidth
-        bins = numpy.arange(0.,3.*max(fNPE)+binwidth,binwidth)  
+        bins = numpy.arange(0.,max(fNPE)+binwidth,binwidth)
+        lastedge = bins[-1]
+        bins = numpy.append(bins, lastedge + 200.*binwidth )
         dataHist,dataEdges = numpy.histogram(fNPE,bins=bins)
+        if debug > 0 : print('calibf.fitExpt bins',bins)
 
         mu = numpy.mean(fNPE)
         if muMC is not None : mu = muMC
@@ -153,6 +186,7 @@ class calibf():
         if PLOT : 
             ### plot results
             X,Y = numpy.array(CFval),numpy.array(chival)
+            npts = len(CFval)
 
             ylabel = r'$\chi^2$'
             if useLogLike : ylabel = '2*loglike'
@@ -160,29 +194,63 @@ class calibf():
             fig, (ax0,ax1,ax2) = plt.subplots(nrows=3,ncols=1)
             ax0.plot(X,Y,'.')
             ax0.plot((bfCF),(chimin),'x',color='red')
-            ax0.set_xlabel('Calibration factor')
+            method = r' $\chi^2$ method'
+            if useLogLike : method = ' logLikelihood method'
+            ax0.set_xlabel('Calibration factor Npoints {}'.format(npts) + method)
             
             ax0.set_ylabel(ylabel)
+            ax0.grid()
 
             ax1.plot(X,Y,'.')
             ax1.plot((bfCF),(chimin),'x',color='red')
             ax1.set_ylim((chimin-20.,chimin+1000.))
-            ax1.set_xlim((max(0.,bfCF-.2),bfCF+.2))
+            xlim = self.defineXlim(X,Y,chimin+1000.)
+            ax1.set_xlim(xlim) #(max(0.,bfCF-.2),bfCF+.2))
             ax1.set_xlabel('Calibration factor')
             ax1.set_ylabel(ylabel)
+            ax1.grid()
 
             ax2.plot(X,Y,'.')
             ax2.plot((bfCF),(chimin),'x',color='red')
-            ax2.set_ylim((chimin-5.,chimin+1000./5.))
-            ax2.set_xlim((max(0.,bfCF-.1),bfCF+.1))
+            ax2.set_ylim((chimin-5.,chimin+200. ))
+            xlim = self.defineXlim(X,Y,chimin+200. )
+            ax2.set_xlim(xlim) #(max(0.,bfCF-.1),bfCF+.1))
             ax2.set_xlabel('Calibration factor')
             ax2.set_ylabel(ylabel)
+            ax2.grid()
 
             plt.tight_layout()
+
+            pdf = self.Figures + words + '.pdf'
+            plt.savefig(pdf)
+            print('calibf.fitResult Wrote',pdf)
+            
             plt.show()
         
         return bfNPE, bfCF
-            
+    def defineXlim(self,Xin,Yin,ymax):
+        '''
+        return abscissa limits such that they contain the most extreme values of Yin<ymax. 
+        input Xin,Yin = arrays; ymax = maximum y value
+
+        '''
+        X,Y = numpy.array(Xin), numpy.array(Yin)
+        igood = []
+        for i,y in enumerate(Y-ymax):
+            if y<0: igood.append(i)
+
+        if len(igood)>0: 
+            i1 = max(0,igood[0]-1)
+            i2 = min(len(X)-1,igood[-1]+1)
+            xlo = X[i1]
+            xhi = X[i2]
+        else:
+            xlo = X[0]
+            xhi = X[-1]
+        xlim = [xlo,xhi]
+        return xlim
+        
+        
     def stats(self,a):
         '''
         return mean, standard deviation, median of input array a
@@ -198,7 +266,7 @@ class calibf():
         y1 = yfactor*(ylim[1]-ylim[0])+ylim[0]
         ax.text(x1,y1,words+r' $\mu$ {:.2f} $\sigma$ {:.2f} $\tilde x$ {:.2f}'.format(mean,std,median))
         return
-    def oneExpt(self,muData=0.63,muMC=0.5,Nevt=1000,PLOT=True,allSteps=False,useLogLike=False):
+    def oneExpt(self,muData=0.63,muMC=0.5,Nevt=1000,PLOT=True,allSteps=False,useLogLike=False,debug=-1,comment=''):
         '''
         generate, analyze and plot one experiment
         given input parameters
@@ -206,11 +274,13 @@ class calibf():
 
         trueCF = muData/muMC
 
+        if comment!='' : print('calibf.oneExpt muData',muData,'muMC',muMC,'trueCF',trueCF,comment)
                 
         mu = muData
         NPE, fNPE = self.genExpt(Nevt,mu,model='DEFAULT')
 
-        bfNPE, bfCF = self.fitExpt(fNPE, model='DEFAULT', muMC=muMC, PLOT=PLOT, allSteps=allSteps, useLogLike=useLogLike)
+        bfNPE, bfCF = self.fitExpt(fNPE, model='DEFAULT', muMC=muMC, PLOT=PLOT, allSteps=allSteps, useLogLike=useLogLike,debug=debug,comment=comment)
+        words = 'oneExpt_muData_{:}_muMC_{:}_Nevt_{:}'.format(int(1000*muData),int(1000*muMC),Nevt) + comment
 
 
         if PLOT : 
@@ -225,26 +295,209 @@ class calibf():
             binwidth = self.binwidth
             bins = numpy.arange(0.,max(max(NPE),max(fNPE))+binwidth,binwidth)
 
-            ax1.set_title('NPE true CF {:.3f} bestfit CF {:.3f}'.format(trueCF,bfCF))
+            method = r' $\chi^2$ method'
+            if useLogLike : method = ' loglikelihood method'
+            ax1.set_title('NPE true CF {:.3f} bestfit CF {:.3f}'.format(trueCF,bfCF) + method)
             Y,binedges = numpy.histogram(fNPE,bins)
             Yerr = numpy.sqrt(Y)
             X = 0.5*(binedges[1:]+binedges[:-1])
             ax1.errorbar(X,Y,fmt='o',color='black',yerr=Yerr,label='NPE mock data')
-            self.writeStats(fNPE,ax1,yfactor=0.55,words='Data')
+            self.writeStats(fNPE,ax1,yfactor=0.6,words='Data')
 
 
             weight = float(len(fNPE))/float(len(bfNPE))
             weights = numpy.full(len(bfNPE),weight)
             ax1.hist(bfNPE,bins,weights=weights,color='blue',label='Best fit',alpha=0.5)
-            self.writeStats(bfNPE,ax1,yfactor=0.4,words='Best fit')
+            self.writeStats(bfNPE,ax1,yfactor=0.3,words='Best fit')
 
             plt.legend(loc='best')        
-            plt.tight_layout()        
+            plt.tight_layout()
+            pdf = self.Figures + words + '.pdf'
+            plt.savefig(pdf)
+            print('calibf.oneExpt Wrote',pdf)
             plt.show()
                 
         return bfCF
+
+    def plotExptData(self,exptData):
+        '''
+        plots results given exptData
+        exptData = {} # {iConfig : [ (Nexpt, Nevt, muData, trueCF), [bfCF0, bfCF1, ...] ] }
+
+        '''
+        
+        print('\ncalibf.plotExptData Plot some stuff')
+        print('calibf.plotExptData sorted(exptData.keys())',sorted(exptData.keys()))
+
+        # repopulate some lists
+        tCF0 = None
+        trueCFlist = []
+        Expts,Events,muDataList = [],[],[]
+        biasLimits = [1.e20,-1.e20]
+        for iConfig in exptData:
+            Nexpt, Nevt, muData, trueCF = exptData[iConfig][0]
+            CFs = exptData[iConfig][1]
+            bias = numpy.array(CFs)-trueCF
+            biasLimits[0] = min(biasLimits[0],min(bias))
+            biasLimits[1] = max(biasLimits[1],max(bias))
+            if trueCF not in trueCFlist : trueCFlist.append( trueCF )
+            if tCF0 is None : tCF0 = trueCF
+            if trueCF==tCF0 :
+                Expts.append( Nexpt )
+                Events.append( Nevt )
+                muDataList.append( muData )
+        #words = 'main_Nexpt_{:}_Nevt_{:}'.format(Nexpt,Nevt)
+
+        binwidth = 0.01/2
+        bins = numpy.arange(biasLimits[0],biasLimits[1]+binwidth,binwidth)
+
+        muDataList = sorted(muDataList)
+        trueCFlist = sorted(trueCFlist)
+        ncols = len(set(trueCFlist))
+        nrows = len(set(muDataList))
+        print('calibf.plotExptData ncols=# trueCF=',ncols,'nrows=# muData=',nrows)
+        fontsize = 6
+        if nrows*ncols>12 : fontsize = 5.5
+                
+        # plot distribution of measured CF - true CF for each set of experiments
+        # also fill dicts used later to plot bias, sigma, etc. 
+        FIG, AX = plt.subplots(nrows=nrows,ncols=ncols,sharex=True,constrained_layout=True, figsize=(1.1*6.4,1.3*4.8)) # (6.4,4.8) =default size in inches width, height matplot3.5.1
+        
+        xlimits = [1.e20,-1.e20]
+        X,Ycf,eYcf,Ycfn,Ystd = {},{},{},{},{}
+        tCF0 = trueCFlist[0]
+        for iCF,trueCF in enumerate(trueCFlist):
+            X[trueCF] = []
+            Ycf[trueCF] = []
+            eYcf[trueCF] = []
+            Ycfn[trueCF] = []
+            Ystd[trueCF] = []
+            for iConfig in exptData:
+                Nexpt, Nevt, muData, tCF = params = exptData[iConfig][0]
+                
+                if trueCF==tCF :
+                    CFs    = exptData[iConfig][1]
+                    CFs = numpy.array( CFs )
+                    bias = CFs - tCF
+                    xlimits[0] = min(xlimits[0],min(bias))
+                    xlimits[1] = max(xlimits[1],max(bias))
+                    iax = muDataList.index(muData)
+                    label = r'X {:} v {:} $\mu$ {:.2f} tCF {:.1f}'.format(*params)
+                    #print('iax',iax,'iCF',iCF,'iConfig',iConfig,label)
+                    AX[iax][iCF].hist(bias,bins=bins)
+                    AX[iax][iCF].set_title(label,fontsize=fontsize)
+                    AX[iax][iCF].grid()
+                    
+                    mean,std = numpy.mean(CFs), numpy.std(CFs)
+                    X[trueCF].append( muData )
+                    Ycf[trueCF].append( mean-trueCF )
+                    eYcf[trueCF].append( std/numpy.sqrt(Nexpt) )
+                    Ystd[trueCF].append( std )
+                    pull = 0.
+                    if std>0.001 :
+                        pull = (mean-trueCF)/std
+                    else:
+                        pull = 0
+                        print('calibf.plotExptData ODD std=',std,'CFs',CFs)
+                        print('calibf.plotExptData ODD',label)
+                    Ycfn[trueCF].append( pull )
+
+        for iax in range(len(AX)):
+            for iCF in range(len(AX[0])):
+                AX[iax][iCF].set_xlim( xlimits )
+                AX[iax][iCF].tick_params(axis='both',which='major', labelsize=8)
+                AX[iax][iCF].tick_params(axis='both',which='minor', labelsize=8)
+                if iax==len(AX)-1 : AX[iax][iCF].set_xlabel('bias',fontsize=8)
+
+
+        pdf = self.Figures + 'toy_CF_bias_table.pdf'
+        plt.savefig(pdf)
+        print('calibf.plotExptData Wrote',pdf)
+        plt.show()
+        
+                        
+        ### plot CF bias and standard deviation vs muData for different values of trueCF
+        fig, (ax0,ax2) = plt.subplots(nrows=2,ncols=1, sharex=True)
+        ax = (ax0,ax2)
+
+        for trueCF in trueCFlist:
+            ax0.errorbar(X[trueCF],Ycf[trueCF],fmt='o-',yerr=eYcf[trueCF], label='trueCF {:.2f}'.format(trueCF))
+            ax0.set_ylabel('CF bias')
+
+            ax2.plot(X[trueCF],Ystd[trueCF],'o-',label='trueCF {:.2f}'.format(trueCF))
+            ax2.set_xlabel('mean NPE')
+            ax2.set_ylabel(r'$\sigma(CF)$')
+            
+        for a in [ax0]:
+            ylim = a.get_ylim()
+            yma = 1.1*max(abs(ylim[0]),abs(ylim[1]))
+            ylim = [-yma,yma]
+            a.set_ylim( ylim )
+        # specifying my favorite tick spacing
+        # taken from https://stackoverflow.com/questions/12608788/changing-the-tick-frequency-on-x-or-y-axis-in-matplotlib/19972993#19972993
+        tick_spacing = 0.1
+        ax0.yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
+
+        ylim = ax2.get_ylim()
+        ylim = [0, 1.1*ylim[1]]
+        ax2.set_ylim( ylim )
+            
+        ax0.legend(loc='best',ncol=2)
+        ax0.grid()
+        ax2.grid()
+        plt.tight_layout()
+
+        pdf = self.Figures + 'toy_CF_bias_sigma_vs_NPE.pdf'
+        plt.savefig(pdf)
+        print('calibf.plotExptData Wrote',pdf)
+        plt.show()
+
+                
+        return
+    def groupExpts(self, timestamps):
+        '''
+        return exptData collated from a number of jobs specified by the input list timestamps
+        
+        exptData = {} # {iConfig : [ (Nexpt, Nevt, muData, trueCF), [bfCF0, bfCF1, ...] ] }
+        '''
+        exptData = None
+        keymax = 0
+        for iloop,timestamp in enumerate(timestamps):
+            if exptData is not None : keymax = max( exptData.keys() ) 
+            eData = self.readPickle(timestamp)
+            if exptData is None:
+                exptData = copy.copy(eData)
+            else:
+                for jConfig in eData:
+                    iConfig = jConfig + keymax
+                    if iConfig in exptData:
+                        print('calibf.groupExpts ERROR iConfig',iConfig,'already exists for jConfig',jConfig,'and keymax',keymax,'for timestamp',timestamp)
+                        sys.exit('calibf.groupExpts ERROR Overwrite of dict')
+                    exptData[iConfig] = eData[jConfig]
+        return exptData
+    def readPickle(self,timestamp):
+        '''
+        return exptData given job specified by timestamp
+        '''
+        pickle_fn = self.rootDir + timestamp + '/' + timestamp + '.pickle'
+
+        f = open(pickle_fn, 'rb')
+        exptData = pickle.load( f)
+        f.close()
+        print('calibf.readPickle Read',pickle_fn)
+
+        return exptData
+    def readAndPlot(self,timestamps):
+        '''
+        read in pickle file specified by timestamps and plot contents
+        '''
+
+        exptData = self.groupExpts(timestamps)
+        self.plotExptData( exptData )
+        return
+        
     def main(self):
-        testSPE = False
+        testSPE = False  ############# TEST THE GENERATION OF SPE DISTRIBUTIONS
         if testSPE : 
             for Ninput in [10,200,5000,50000]:
                 response = self.SPE(Ninput)
@@ -254,24 +507,36 @@ class calibf():
 
         showOne = False
         if showOne :
-            Nevt = 1000
-            for useLogLike in [False,True]:
+            Nevt = 10000
+            trueCF = 0.5
+            trueCF = 1.0
+            njob = 0
+            for useLogLike in [False] : 
                 for allSteps in [False,True]:
-                    for muData in [ 0.5, 1.5, 15.]:
+                    for muData in [ 0.5, 5.0, 15.]:
+                        comment = 'Njob_{:}'.format(njob)
+                        njob += 1
                         print('calibf.main showOne Nevt,muData',Nevt,muData,'useLogLike',useLogLike)
-                        bfCF = self.oneExpt(muData=muData,muMC=1.3*muData,Nevt=Nevt,PLOT=True,allSteps=allSteps)
+                        bfCF = self.oneExpt(muData=muData,muMC=muData/trueCF,Nevt=Nevt,PLOT=True,allSteps=allSteps,debug=1,useLogLike=useLogLike,comment=comment)
             sys.exit('calibf.main Completed showOne')
         
 
         
         exptData = {} # {iConfig : [ (Nexpt, Nevt, muData, trueCF), [bfCF0, bfCF1, ...] ] }
         
-        Nevt = 1000
-        Nexpt = 50
+
         iConfig = 0
         trueCFlist = [0.7, 1.4]
+
+        trueCFlist = [1.0]
+
+        trueCFlist = [0.4, 1.7]
         muDataList = [0.5, 1.5, 5.0, 10., 15.]
-        for muData in muDataList: 
+        Expts      = [100, 100, 50,  20,  20]
+        Events     = [10000,10000, 10000, 10000, 10000]
+        for jExpt,muData in enumerate(muDataList):
+            Nevt = Events[jExpt]
+            Nexpt= Expts[jExpt]
             for trueCF in trueCFlist:
                 muMC = muData / trueCF
                 iConfig += 1
@@ -288,49 +553,28 @@ class calibf():
                 pull  = 99.
                 if std>0. : pull = delta/std
                 print('calibf.main iConfig',iConfig,'best fit CF mean,stddev,mean-true,pull {:.3f} {:.3f} {:.3f} {:.3f}'.format(mean,std,delta,pull))
-        # plot bias on CF vs mean NPE
-        print('\ncalibf.main Plot some stuff')
-        X,Ycf,Ycfn = {},{},{}
-        for trueCF in trueCFlist:
-            X[trueCF] = []
-            Ycf[trueCF] = []
-            Ycfn[trueCF] = []
-            for iConfig in exptData:
-                Nexpt, Nevt, muData, tCF = params = exptData[iConfig][0]
-                #print('calibf.main trueCF',trueCF,'iConfig',iConfig,'Nexpt {:} Nevt {:} muData {:.2f} tCF {:.1f}'.format(*params))
-                
-                if trueCF==tCF :
-                    CFs    = exptData[iConfig][1]
-                    mean,std = numpy.mean(CFs), numpy.std(CFs)
-                    X[trueCF].append( muData )
-                    Ycf[trueCF].append( mean-trueCF )
-                    pull = 0.
-                    if std>0.001 :
-                        pull = (mean-trueCF)/std
-                    else:
-                        pull = 0
-                        print('calibf.main ODD std=',std,'CFs',CFs)
-                    Ycfn[trueCF].append( pull )
 
-        #print('calibf.main X',X,'Ycf',Ycf,'Ycfn',Ycfn)
-        fig, (ax0,ax1) = plt.subplots(nrows=2,ncols=1)
-        for trueCF in trueCFlist:
-            ax0.plot(X[trueCF],Ycf[trueCF],'o',label='trueCF {:.2f}'.format(trueCF))
-            ax0.set_xlabel('mean NPE')
-            ax0.set_ylabel('CF bias')
+        pickle_fn = self.logDir + '/' + self.now + '.pickle'
+        f = open(pickle_fn, 'wb')
+        pickle.dump( exptData, f)
+        f.close()
+        print('calibf.main Wrote',pickle_fn)
 
-            ax1.plot(X[trueCF],Ycfn[trueCF],'o',label='trueCF {:.2f}'.format(trueCF))
-            ax1.set_xlabel('mean NPE')
-            ax1.set_ylabel('CF pull')
-        ax0.legend(loc='best')
-        ax0.grid()
-        ax1.legend(loc='best')
-        ax1.grid()
-        plt.tight_layout()
-        plt.show()
-
+        self.plotExptData(exptData)
+        
                 
         return
 if __name__ == '__main__' :
     P = calibf()
-    P.main()
+
+#20220316T153733 20220317T134917 20220317T090549
+# [0.7, 1.4]      [0.4, 1.7]      [1.0]  = trueCF
+    
+    timestamps = None
+    if len(sys.argv)>1 :
+        timestamps = [a for a in sys.argv[1:]]
+
+    if timestamps is not None :
+        P.readAndPlot(timestamps)
+    else:
+        P.main()
