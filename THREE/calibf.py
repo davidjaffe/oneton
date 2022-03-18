82b6bc0cc217f349aacf8e81bfd69ee953d9daf9
David Jaffe <david.ezekial.jaffe@gmail.com>
2022-03-18 15:28:26 -0400

    NEWgenExpt puts in some bkgd

--- THREE/calibf.py
+++ THREE/calibf.py
@@ -103,17 +103,53 @@ class calibf():
             fNPE_gen.append( fNPE )
         
         return numpy.array(NPE_gen), numpy.array(fNPE_gen)
+    def NEWgenExpt(self,Nevt,mu,model='DEFAULT',Nbkg=None, beta=None):
+        '''
+        return NPE_gen, fNPE_gen for Nevt events with mean NPE mu and model as specified.
+        where NPE_gen = array of integer number of photoelectrons, and
+        fNPE_gen = array of float number of photoelectrons (with electronics response included)
+
+        model = 'DEFAULT' = poisson distribution with mean mu
+        model = 'BKGD1'   = (Nevt-Nbkg) events from poisson distribution with mean mu, Nbkg events from exponential exp(-x/beta)/beta
+        '''
+
+        N = Nevt
+        model1 = model
+        if model=='BKGD1' :
+            model1 = 'DEFAULT'
+            if Nbkg is None or beta is None :
+                sys.exit('calibf.NEWgenExpt ERROR model='+model,' but required parameters Nbkg='+str(Nbkg)+' and beta='+str(beta))
+            N = Nevt - Nbkg
+        
+        NPE_gen, fNPE_gen = [],[]
+        for i in range(N):
+            NPE, fNPE = self.genEvent(mu,model=model1)
+            NPE_gen.append( NPE )
+            fNPE_gen.append( fNPE )
+
+        if model=='BKGD1' :
+            fNPE_gen.extend( list( numpy.random.exponential(beta,Nbkg) ) )
+        
+        return numpy.array(NPE_gen), numpy.array(fNPE_gen)
     def fitExpt(self,fNPE,model='DEFAULT',muMC=None,PLOT=True,allSteps=False,useLogLike=False,debug=-1,comment=''):
         '''
         return bfNPE, cf given input NPE distribution fNPE where
         bfNPE = best fit NPE distribution
         cf  = calibration factor applied to MC to give best fit
-        '''
-        
-        meanmu = numpy.mean(fNPE)
-        words = 'fitExpt_meanData_{:}_meanMC_{:}permille'.format(int(1000*meanmu),int(1000*muMC)) + comment
-        if comment!='' : print('calibf.fitExpt',words)
 
+        inputs
+        fNPE = one experiment's of events. each event is NPE with electronics response applied
+        model = model to be used to generate 'MC' distribution. 
+        muMC = mean PE for 'MC' distribution, if None, use mean(fNPE)
+        PLOT = produce plots showing scan of chi2 vs CF
+        allSteps = if True, take fixed steps between limits, if False, use iterative procedure
+        useLogLike = if False, use chisquare method, if True, use logLikelihood 
+        debug > 0 generates output
+        comment is text to be used for output file (if PLOT=True)
+
+        '''
+
+        ## define scan method
         minCF = 0.2
         maxCF = 2.2
         if allSteps :
@@ -139,10 +175,17 @@ class calibf():
         dataHist,dataEdges = numpy.histogram(fNPE,bins=bins)
         if debug > 0 : print('calibf.fitExpt bins',bins)
 
+        ## generate 'MC' experiment
         mu = numpy.mean(fNPE)
         if muMC is not None : mu = muMC
         NPE, mcNPE = self.genExpt(NMC,mu,model=model)
         
+        
+        meanmu = numpy.mean(fNPE)
+        words = 'fitExpt_meanData_{:}_meanMC_{:}permille'.format(int(1000*meanmu),int(1000*mu)) + comment
+        if comment!='' : print('calibf.fitExpt',words)
+
+
         chimin = 1.e20
         bfCF     = None
         bfNPE  = None
@@ -161,8 +204,6 @@ class calibf():
                             chisqr += d*numpy.log(d/m)
                     chisqr = 2.*chisqr
                 else: 
-                    #sigsqr = dataHist + MChist/rmcd/rmcd
-                    #chisqr = numpy.sum( (dataHist - MChist)**2/sigsqr )
                     chisqr = 0.
                     nterm = 0
                     for d,m in zip(dataHist,MChist):
@@ -184,7 +225,7 @@ class calibf():
         bfNPE = bfCF * mcNPE
 
         if PLOT : 
-            ### plot results
+            ### plot results. Show full scan and then two progressively finer zoom-ins around minimum
             X,Y = numpy.array(CFval),numpy.array(chival)
             npts = len(CFval)
 
@@ -205,7 +246,7 @@ class calibf():
             ax1.plot((bfCF),(chimin),'x',color='red')
             ax1.set_ylim((chimin-20.,chimin+1000.))
             xlim = self.defineXlim(X,Y,chimin+1000.)
-            ax1.set_xlim(xlim) #(max(0.,bfCF-.2),bfCF+.2))
+            ax1.set_xlim(xlim) 
             ax1.set_xlabel('Calibration factor')
             ax1.set_ylabel(ylabel)
             ax1.grid()
@@ -214,7 +255,7 @@ class calibf():
             ax2.plot((bfCF),(chimin),'x',color='red')
             ax2.set_ylim((chimin-5.,chimin+200. ))
             xlim = self.defineXlim(X,Y,chimin+200. )
-            ax2.set_xlim(xlim) #(max(0.,bfCF-.1),bfCF+.1))
+            ax2.set_xlim(xlim) 
             ax2.set_xlabel('Calibration factor')
             ax2.set_ylabel(ylabel)
             ax2.grid()
@@ -249,8 +290,6 @@ class calibf():
             xhi = X[-1]
         xlim = [xlo,xhi]
         return xlim
-        
-        
     def stats(self,a):
         '''
         return mean, standard deviation, median of input array a
@@ -266,10 +305,19 @@ class calibf():
         y1 = yfactor*(ylim[1]-ylim[0])+ylim[0]
         ax.text(x1,y1,words+r' $\mu$ {:.2f} $\sigma$ {:.2f} $\tilde x$ {:.2f}'.format(mean,std,median))
         return
-    def oneExpt(self,muData=0.63,muMC=0.5,Nevt=1000,PLOT=True,allSteps=False,useLogLike=False,debug=-1,comment=''):
+    def oneExpt(self,muData=0.63,muMC=0.5,Nevt=1000,PLOT=True,allSteps=False,useLogLike=False,debug=-1,comment='',model='DEFAULT'):
         '''
         generate, analyze and plot one experiment
+        return bfCF = best fit calibration factor
         given input parameters
+        muData = mean of poisson distribution for fake Data
+        muMC   = mean of poisson dist for 'MC'
+        Nevt = events to generate for this experiment
+        PLOT = if True, show generated and fitted fake data distribution
+        allSteps, useLogLike = Inputs to fitExpt
+        debug > 0 generates output
+        comment = text to append to file name if PLOT=True
+        model = model used for generation of NPE distributions
         '''
 
         trueCF = muData/muMC
@@ -277,13 +325,17 @@ class calibf():
         if comment!='' : print('calibf.oneExpt muData',muData,'muMC',muMC,'trueCF',trueCF,comment)
                 
         mu = muData
-        NPE, fNPE = self.genExpt(Nevt,mu,model='DEFAULT')
+        NPE, fNPE = self.genExpt(Nevt,mu,model=model)
 
-        bfNPE, bfCF = self.fitExpt(fNPE, model='DEFAULT', muMC=muMC, PLOT=PLOT, allSteps=allSteps, useLogLike=useLogLike,debug=debug,comment=comment)
+        bfNPE, bfCF = self.fitExpt(fNPE, model=model, muMC=muMC, PLOT=PLOT, allSteps=allSteps, useLogLike=useLogLike,debug=debug,comment=comment)
         words = 'oneExpt_muData_{:}_muMC_{:}_Nevt_{:}'.format(int(1000*muData),int(1000*muMC),Nevt) + comment
 
 
-        if PLOT : 
+        if PLOT :
+            ## two panels:
+            ## top panel is fake data NPE distribution w/o electronics effects
+            ## bottom panel is fake data NPE dist with electronics effects applied
+            ##      overlaid with best fit NPE distribution from 'MC'
             binwidth = 1.
             bins = numpy.arange(0.,max(max(NPE),max(fNPE))+binwidth,binwidth)
 
@@ -304,7 +356,6 @@ class calibf():
             ax1.errorbar(X,Y,fmt='o',color='black',yerr=Yerr,label='NPE mock data')
             self.writeStats(fNPE,ax1,yfactor=0.6,words='Data')
 
-
             weight = float(len(fNPE))/float(len(bfNPE))
             weights = numpy.full(len(bfNPE),weight)
             ax1.hist(bfNPE,bins,weights=weights,color='blue',label='Best fit',alpha=0.5)
@@ -318,11 +369,12 @@ class calibf():
             plt.show()
                 
         return bfCF
-
     def plotExptData(self,exptData):
         '''
-        plots results given exptData
+        plots results given dict exptData
         exptData = {} # {iConfig : [ (Nexpt, Nevt, muData, trueCF), [bfCF0, bfCF1, ...] ] }
+        iConfig is key that distinguishes between different configuations specified by parameters
+        (Nexpt, Nevt, muData, trueCF). [bfCF0, bfCF1, ..., bfCFi, ...] are the best-fit CF from experiments 0, 1, ..., i, ... 
 
         '''
         
@@ -346,7 +398,7 @@ class calibf():
                 Expts.append( Nexpt )
                 Events.append( Nevt )
                 muDataList.append( muData )
-        #words = 'main_Nexpt_{:}_Nevt_{:}'.format(Nexpt,Nevt)
+
 
         binwidth = 0.01/2
         bins = numpy.arange(biasLimits[0],biasLimits[1]+binwidth,binwidth)
@@ -360,8 +412,9 @@ class calibf():
         if nrows*ncols>12 : fontsize = 5.5
                 
         # plot distribution of measured CF - true CF for each set of experiments
-        # also fill dicts used later to plot bias, sigma, etc. 
-        FIG, AX = plt.subplots(nrows=nrows,ncols=ncols,sharex=True,constrained_layout=True, figsize=(1.1*6.4,1.3*4.8)) # (6.4,4.8) =default size in inches width, height matplot3.5.1
+        # also fill dicts used later to plot bias, sigma, etc.
+        ### # figsize=(6.4,4.8) =default figure size in inches width, height matplot3.5.1
+        FIG, AX = plt.subplots(nrows=nrows,ncols=ncols,sharex=True,constrained_layout=True, figsize=(1.1*6.4,1.3*4.8)) 
         
         xlimits = [1.e20,-1.e20]
         X,Ycf,eYcf,Ycfn,Ystd = {},{},{},{},{}
@@ -383,7 +436,6 @@ class calibf():
                     xlimits[1] = max(xlimits[1],max(bias))
                     iax = muDataList.index(muData)
                     label = r'X {:} v {:} $\mu$ {:.2f} tCF {:.1f}'.format(*params)
-                    #print('iax',iax,'iCF',iCF,'iConfig',iConfig,label)
                     AX[iax][iCF].hist(bias,bins=bins)
                     AX[iax][iCF].set_title(label,fontsize=fontsize)
                     AX[iax][iCF].grid()
@@ -433,7 +485,8 @@ class calibf():
             yma = 1.1*max(abs(ylim[0]),abs(ylim[1]))
             ylim = [-yma,yma]
             a.set_ylim( ylim )
-        # specifying my favorite tick spacing
+            
+        # specifying my favorite tick spacing for bias axis (ordinate)
         # taken from https://stackoverflow.com/questions/12608788/changing-the-tick-frequency-on-x-or-y-axis-in-matplotlib/19972993#19972993
         tick_spacing = 0.1
         ax0.yaxis.set_major_locator(ticker.MultipleLocator(tick_spacing))
@@ -489,7 +542,7 @@ class calibf():
         return exptData
     def readAndPlot(self,timestamps):
         '''
-        read in pickle file specified by timestamps and plot contents
+        read in pickle files specified by timestamps, collate info and plot contents
         '''
 
         exptData = self.groupExpts(timestamps)
@@ -503,9 +556,38 @@ class calibf():
                 response = self.SPE(Ninput)
                 response = numpy.array( response )
                 print('calibf.main Ninput',Ninput,'len(response)',len(response),'sum(response<0.)',sum(response<0.),'sum(response)/float(len(response))',sum(response)/float(len(response)))
+                sys.exit('calibf.main Completed testSPE')
 
+        testBkgd = False
+        if testBkgd :
+            Nevt = 10000
+            Nbkg =   int(.02*Nevt)
+            mu = 12.
+            beta = 25.
+            model = 'BKGD1'
 
-        showOne = False
+            NPE, fNPE = self.NEWgenExpt(Nevt,mu,model=model,Nbkg=Nbkg,beta=beta)
+            binwidth = 1.
+            bins = numpy.arange(0.,max(NPE)+2.*binwidth,binwidth)
+            ovflow = bins[-2]+0.01*binwidth
+            fNPE_trimmed = numpy.clip(fNPE,0.,ovflow)
+            fig, (ax0,ax1) = plt.subplots(nrows=2,ncols=1)
+            ax0.hist(NPE,bins=bins,label='NPE w/o bkgd or electronics response')
+            ax0.grid()
+            ax0.legend(loc='best')
+            
+            ax1.hist(fNPE_trimmed,bins=bins,label='NPE w bkgd and electronics response')
+            for bar in ax1.containers[0]:
+                if bar.get_x() <= ovflow <= bar.get_x()+bar.get_width() : bar.set_color('red')
+            ax1.grid()
+            ax1.legend(loc='best')
+            
+            plt.tight_layout()
+            plt.show()
+            sys.exit('calibf.main Completed testBkgd ---------------------------------------')
+            
+
+        showOne = False   ############# SHOW SINGLE EXPERIMENT RESULT (chi2 scan and NPE distributions)
         if showOne :
             Nevt = 10000
             trueCF = 0.5
