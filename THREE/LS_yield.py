b1c18e467293f3825d4ea8354751c6d7a49d8445
David Jaffe <david.ezekial.jaffe@gmail.com>
2023-01-04 11:57:23 -0500

    plot relative LY for different msmts

--- THREE/LS_yield.py
+++ THREE/LS_yield.py
@@ -22,6 +22,8 @@ from scipy.optimize import curve_fit
 class LS_yield():
     def __init__(self):
 
+        self.figDir = 'LS_YIELD_FIGURES/'
+
         # 1508.07029 WbLS LY contents of table 4
         self.BNLabsLY = {'0.4%': (19.9, 2.3),
                          '1%'  : (108.9, 10.9),
@@ -70,8 +72,8 @@ class LS_yield():
         # results from table 2
         self.LY4 = {'EJ301'     : [ 'NA', (78.,0.), 13570],
                     'p-Xylene'  : [ '4 g/L PPO', (77.2,1.9), 13430],
-                    'LAB'       : [ '4 g/L PPO', (60.2,1.4), 10470],
-                    'DIN'       : [ '4 g/L PPO', (70.0,1.7), 12190],
+                    'LAB40'     : [ '4 g/L PPO', (60.2,1.4), 10470],
+                    'DIN40'     : [ '4 g/L PPO', (70.0,1.7), 12190],
                     'relativeTo': ['anthracene']}
 
         # NIM A701 (2013) 133
@@ -93,7 +95,7 @@ class LS_yield():
                     'LAB10'  : ['1.0 g/L PPO', (0.46, 0.01)],
                     'LAB20'  : ['2.0 g/L PPO', (0.64, 0.01)],
                     'LAB30'  : ['3.0 g/L PPO', (0.67, 0.01)],
-                    'LAB40est':['4.0 g/L PPO', (0.70, 0.01)],
+#                    'LAB40est':['4.0 g/L PPO', (0.70, 0.01)],
                     'LAB50'  : ['5.0 g/L PPO', (0.72, 0.01)],
                     'relativeTo': ['EJ301']}
 
@@ -112,9 +114,11 @@ class LS_yield():
         if s=='NA' : return 0.
         return float(s)
         
-    def unpackRef(self,ref):
+    def unpackRef(self,ref,solvent='LAB',report=False):
         '''
-        return LS names, WLS conc, relative LY and reference LS
+        return relTo, X,Y,dY,Z  = reference LS, WLS conc, relative LY, uncertainty on relative light yield, absolute LY
+        relTo is a string
+        X, Y, dY, Z are numpy arrays. Z can be zero length. 
         given ref
         '''
         if ref not in self.LY :
@@ -124,9 +128,10 @@ class LS_yield():
         relTo = None
         block = self.LY[ref]
         for LS in block:
+            
             if LS=='relativeTo' :
                 relTo = block[LS][0]
-            else:
+            elif solvent in LS:
                 conc = block[LS][0]
                 v,dv = block[LS][1]
                 if len(block[LS])> 2: z.append( block[LS][2] )
@@ -134,20 +139,137 @@ class LS_yield():
                 y.append( v )
                 dy.append( dv )
         if relTo is None: sys.exit('LS_yield.unpackRef ERROR No relativeTo for ref '+ ref)
-        X = self.toNPA(x)
-        Y = self.toNPA(y)
-        dY= self.toNPA(dy)
-        Z = self.toNPA(z)
+            
+        X,Y,dY,Z = self.toNPA(x),self.toNPA(y),self.toNPA(dy),self.toNPA(z)
+        
+        if report : self.reportUnp(ref,solvent,relTo, X,Y,dY,Z)
         return relTo, X,Y,dY,Z 
-    
     def toNPA(self,a):
         return numpy.array(a)
     def linear(self, x, m, b):
         return m*x + b
+    def reportUnp(self,ref,solvent,relTo,X,Y,dY,Z):
+        '''
+        report results of unpacking
+        '''
+        words = ''
+        if len(X)==0: words = 'NO ENTRIES'
+        print('LS_yield.reportUnp ref',ref,'solvent',solvent,'relTo',relTo,words)
+        if len(X)>0:
+            print('LS_yield.reportUnp conc',X)
+            print('LS_yield.reportUnp rLY ',Y)
+            print('LS_yield.reportUnp drLY',dY)
+        if len(Z)>0 : print('LS_yield.reportUnp aLY',Z)
+        return
+    def makeRatio(self,ref,sol1,sol2,report=False):
+        '''
+        for ref return ratio of response of solvent sol1 to solvent sol2
+        same variables returned as unpackRef
+        '''
+        Num = relTo, X,Y,dY,Z = self.unpackRef(ref,solvent=sol1,report=report)
+        Den = relTo, X,Y,dY,Z = self.unpackRef(ref,solvent=sol2,report=report)
+
+#        if len(X)!=1: sys.exit('LS_yield.makeRatio ERROR '+str(len(X))+' references, only 1 allowed')
+        
+        relTo = sol2
+        x,y,dy,z = [],[],[],[]
+        Xden,Yden,dYden = Den[1],Den[2],Den[3]
+        Xnum,Ynum,dYnum = Num[1],Num[2],Num[3]
+        
+        for i,pair in enumerate(zip(Ynum,dYnum)):
+            if len(Xden)>1 and abs(Xden[i]-Xnum[i])>1.e-6 :
+                print('LS_yield.makeRatio ERROR i',i,'Xden',Xden[i],'Xnum[i]',Xnum[i])
+                sys.exit('LS_yield.makeRatio ERROR concentrations not equal')
+            v,dv = Yden[i],dYden[i]
+            u,du = pair
+            r = u/v
+            dr= r*math.sqrt(du*du/u/u + dv*dv/v/v)
+            y.append( r )
+            dy.append(dr)
+            x.append( Xnum[i] )
+            
+        X,Y,dY,Z = self.toNPA(x),self.toNPA(y),self.toNPA(dy),self.toNPA(z)
+
+        ratio = sol1 
+        if report : self.reportUnp(ref,ratio,relTo, X,Y,dY,Z)
+
+        return relTo, X,Y,dY,Z        
+    def makeName(self,a,b,c):
+        s = a
+        if len(b)>0 : s+= '_' + b
+        if len(c)>0 : s+= '_' + c
+        return s
     def main(self):
 
+
+        # ref3 = LAB, DIN relative to PC
+        # ref4 = LAB, DIN, EJ301 relative to anthracene
+        # ref5 = LAB relative to EJ301
+
+        # put unpacked results into dict C
+        C = {}
+        report = True
+        for ref in ['Ref3','Ref4','Ref5']:
+            for solvent in ['EJ301','LAB','DIN']:
+                C[self.makeName(ref,solvent,'')] = relTo, X,Y,dY,Z = self.unpackRef(ref,solvent=solvent,report=report)
+
+        ##### plot LAB relative to DIN vs concentration
         ref = 'Ref4'
-        relTo, X,Y,dY,Z = self.unpackRef(ref)
+        sol1,sol2 = 'LAB','DIN'
+        label = self.makeName(ref,sol1,sol2)
+        C[label] = self.makeRatio(ref,sol1,sol2,report=report)
+        relToA,X,Y,dY,Z = C[label]
+        plt.errorbar(X,Y,yerr=dY,marker='o',color='black',linestyle='dotted',label=label)
+
+        ref = 'Ref3'
+        sol1,sol2 = 'LAB','DIN'
+        label = self.makeName(ref,sol1,sol2)
+        C[label] = self.makeRatio(ref,sol1,sol2,report=report)
+        relToB,X,Y,dY,Z = C[label]
+        plt.errorbar(X,Y,yerr=dY,marker='o',color='blue',linestyle='dotted',label=label)
+
+        if relToA!=relToB : sys.exit('LS_yield.main ERROR relToA '+relToA+' not equal to '+relToB)
+        title = 'LAB light yield relative to '+relToA
+        plt.title(title)
+        plt.xlabel('PPO concentration in %')
+        plt.ylabel('Relative light yield')
+        plt.ylim(0.,1.3)
+        plt.grid()
+        plt.legend(loc='best')
+        pdf = self.figDir + title.replace(' ','_')
+        plt.savefig(pdf)
+        print('LS_yield.main Wrote',pdf)
+        plt.show()
+        
+        ##### plot LABPPO relative to EJ301
+        ref = 'Ref4'
+        sol1,sol2 = 'LAB','EJ301'
+        C[self.makeName(ref,sol1,sol2)] = self.makeRatio(ref,sol1,sol2,report=report)
+        
+        label = self.makeName('Ref5','LAB','')
+        relToA,X,Y,dY,Z = C[label]
+        plt.errorbar(X,Y,yerr=dY,marker='o',color='black',linestyle='dotted',label=label)
+
+        label = self.makeName('Ref4','LAB','EJ301')
+        relToB,X,Y,dY,Z = C[label]
+        plt.errorbar(X,Y,yerr=dY,marker='o',color='blue',linestyle='dotted',label=label)
+
+        if relToA!=relToB : sys.exit('LS_yield.main ERROR relToA '+relToA+' not equal to '+relToB)
+
+        title = 'LAB light yield relative to '+relToA
+        plt.title(title)
+        plt.xlabel('PPO concentration in %')
+        plt.ylabel('Relative light yield')
+        plt.ylim(0.,1.01)
+        plt.grid()
+        plt.legend(loc='best')
+        pdf = self.figDir + title.replace(' ','_')
+        plt.savefig(pdf)
+        print('LS_yield.main Wrote',pdf)
+        plt.show()
+
+        
+
         return
 
     def OLDmain(self):
